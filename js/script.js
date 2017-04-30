e1733b293ff9b940c85d51e319b878b211e2b1ab
jnijeremy <nizeyu123@gmail.com>
2017-04-30 15:25:55 -0700

    Implement login function.

--- js/script.js
+++ js/script.js
@@ -1,18 +1,14 @@
 $(document).ready(function() {
     // console.log("lalala");
-
     $("#signupBtn").click(function(event) {
         // console.log(event);
         event.preventDefault();
-
         var username = $("#username").val();
         var password = $("#password").val();
         // console.log(username + " " + password);
-
         if (username && password) {
             // AJAX
-            $.post("http://open-commerce.herokuapp.com/api/signup",
-                {
+            $.post("http://open-commerce.herokuapp.com/api/signup", {
                     username: username,
                     password: password
                 },
@@ -26,6 +22,30 @@ $(document).ready(function() {
                 });
         } else {
             alert("please provide a username and password for signup!");
+        }
+    });
+
+    $("#loginBtn").click(function(event) {
+        event.preventDefault();
+        var username = $("#username").val();
+        var password = $("#password").val();
+        if (username && password) {
+            $.post("http://open-commerce.herokuapp.com/api/login", {
+                    username: username,
+                    password: password
+                },
+                function(response) {
+                    // console.log(response);
+                    if (response.success) {
+                        var cookie = "x-access-token=" + response.token;
+                        document.cookie = cookie;
+                        window.location.href = "/index.html";
+                    } else {
+                        alert(response.message);
+                    }
+                });
+        } else {
+            alert("please provide a username and password for login!");
         }
     });
 });
